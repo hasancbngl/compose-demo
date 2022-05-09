da8da0496e890d1daff3d19b850de8b73cd30644
Hasan Cobanoglu <hasan.cobanoglu1@outlook.com.tr>
2022-05-09 19:04:25 +0300

    update gradle for simple text app

--- Simpletextcomposeexample/app/src/main/java/com/hasan/simpletextcomposeexample/MainActivity.kt
+++ Simpletextcomposeexample/app/src/main/java/com/hasan/simpletextcomposeexample/MainActivity.kt
@@ -3,12 +3,16 @@ package com.hasan.simpletextcomposeexample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.hasan.simpletextcomposeexample.ui.theme.SimpleTextComposeExampleTheme
 
@@ -31,7 +35,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(
+        text = "Hello $name!",
+        Modifier.background(Color.Green, RectangleShape),
+        color = Color.Red,
+        textAlign = TextAlign.Center,
+    )
 }
 
 @Preview(showBackground = true)
