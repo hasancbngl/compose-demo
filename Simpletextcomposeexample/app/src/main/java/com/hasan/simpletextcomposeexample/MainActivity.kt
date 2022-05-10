4416e5a207fd92651a21b32f6998c7b18034befe
Hasan Cobanoglu <hasan.cobanoglu1@outlook.com.tr>
2022-05-10 16:09:47 +0300

    play with buttons and text composobles

--- Simpletextcomposeexample/app/src/main/java/com/hasan/simpletextcomposeexample/MainActivity.kt
+++ Simpletextcomposeexample/app/src/main/java/com/hasan/simpletextcomposeexample/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.hasan.simpletextcomposeexample
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -14,39 +14,41 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import com.hasan.simpletextcomposeexample.ui.theme.SimpleTextComposeExampleTheme
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SimpleTextComposeExampleTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            GreetingButton()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
+fun GreetingText(name: String) {
     Text(
         text = "Hello $name!",
-        Modifier.background(Color.Green, RectangleShape),
+        modifier = Modifier.background(shape = RectangleShape, color = Color.White),
         color = Color.Red,
         textAlign = TextAlign.Center,
+        fontSize = 18.sp
     )
+}
+
+@Composable
+fun GreetingButton() {
+    Button(onClick = {
+        Log.e("TAG", "GreetingButton: ", )
+        //Toast.makeText(, "hey there", Toast.LENGTH_SHORT).show()
+    },
+    shape = CircleShape) {
+        GreetingText(name = "button")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    SimpleTextComposeExampleTheme {
-        Greeting("Android")
-    }
+    GreetingButton()
 }
