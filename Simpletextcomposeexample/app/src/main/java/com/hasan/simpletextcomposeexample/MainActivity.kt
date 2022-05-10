7cd8b7991fa877f83f7f42dd9ae288af7cb11739
Hasan Cobanoglu <hasan.cobanoglu1@outlook.com.tr>
2022-05-10 16:23:51 +0300

    play with buttons and text composobles

--- Simpletextcomposeexample/app/src/main/java/com/hasan/simpletextcomposeexample/MainActivity.kt
+++ Simpletextcomposeexample/app/src/main/java/com/hasan/simpletextcomposeexample/MainActivity.kt
@@ -5,22 +5,35 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.AbsoluteAlignment
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            GreetingButton()
+            Box(
+                modifier = Modifier
+                    .background(Color.Yellow)
+            ) {
+                GreetingButton()
+            }
         }
     }
 }
@@ -38,11 +51,13 @@ fun GreetingText(name: String) {
 
 @Composable
 fun GreetingButton() {
-    Button(onClick = {
-        Log.e("TAG", "GreetingButton: ", )
-        //Toast.makeText(, "hey there", Toast.LENGTH_SHORT).show()
-    },
-    shape = CircleShape) {
+    Button(
+        onClick = {
+            Log.e("TAG", "GreetingButton: ")
+            //Toast.makeText(, "hey there", Toast.LENGTH_SHORT).show()
+        },
+        shape = CircleShape
+    ) {
         GreetingText(name = "button")
     }
 }
@@ -50,5 +65,10 @@ fun GreetingButton() {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    GreetingButton()
+    Box(
+        modifier = Modifier
+            .background(Color.Yellow)
+    ) {
+        GreetingButton()
+    }
 }
