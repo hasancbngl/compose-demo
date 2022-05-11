56254572794cf46ec83333f14c73ce95660c2294
Hasan Cobanoglu <hasan.cobanoglu1@outlook.com.tr>
2022-05-11 13:32:27 +0300

    use surface

--- CoreUIElementsapp/app/src/main/java/com/hasan/coreuielementsapp/MainActivity.kt
+++ CoreUIElementsapp/app/src/main/java/com/hasan/coreuielementsapp/MainActivity.kt
@@ -5,14 +5,18 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.hasan.coreuielementsapp.ui.theme.CoreUIElementsAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,9 +36,12 @@ fun MainScreen() {
     ) {
         Surface(
             color = Color.Red,
-            modifier = Modifier.wrapContentSize()
+            modifier = Modifier
+                .wrapContentSize(align = Alignment.TopCenter)
+                .padding(0.dp, 24.dp, 0.dp, 0.dp)
         ) {
             Text(
+                modifier = Modifier.padding(4.dp),
                 text = "Wrapped Content",
                 color = Color.White,
                 style = MaterialTheme.typography.headlineMedium
