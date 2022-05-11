797010948df0be2515033d3377c89ee6cb2a7ccb
Hasan Cobanoglu <hasan.cobanoglu1@outlook.com.tr>
2022-05-11 09:46:13 +0300

    use modifiers

--- Simpletextcomposeexample/app/src/main/java/com/hasan/simpletextcomposeexample/MainActivity.kt
+++ Simpletextcomposeexample/app/src/main/java/com/hasan/simpletextcomposeexample/MainActivity.kt
@@ -4,11 +4,11 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -19,6 +19,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -28,24 +29,31 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Box(
-                modifier = Modifier
-                    .background(Color.Yellow)
-            ) {
-                GreetingButton()
-            }
+            GreetingText(name = "World!")
         }
     }
 }
 
 @Composable
 fun GreetingText(name: String) {
+    //fillMaxWidth / fillMaxHeight can use percentages
+    //size(width, height) shorter usage of .width and .height
+    //.clickable modifier adds onClick
+    //compose modifiers order matters
+
     Text(
         text = "Hello $name!",
-        modifier = Modifier.background(shape = RectangleShape, color = Color.White),
+        modifier = Modifier
+            .width(200.dp)
+            .height(250.dp)
+            .background(color = Color.Yellow)
+            .border(2.dp, shape = RectangleShape, color = Color.Black)
+            .padding(start = 16.dp)
+            .clickable { Log.e("TAG", "GreetingText: ") },
         color = Color.Red,
         textAlign = TextAlign.Center,
-        fontSize = 18.sp
+        fontSize = 18.sp,
+        fontStyle = FontStyle.Italic
     )
 }
 
@@ -65,10 +73,5 @@ fun GreetingButton() {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    Box(
-        modifier = Modifier
-            .background(Color.Yellow)
-    ) {
-        GreetingButton()
-    }
+    GreetingText(name = "World!")
 }
