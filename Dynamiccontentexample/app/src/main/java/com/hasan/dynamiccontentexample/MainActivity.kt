f1dc3359ed45ee5da9c09c1a1059b24aeba4424c
Hasan Cobanoglu <hasan.cobanoglu1@outlook.com.tr>
2022-05-13 15:44:06 +0300

    add textfield and lift up state

--- Dynamiccontentexample/app/src/main/java/com/hasan/dynamiccontentexample/MainActivity.kt
+++ Dynamiccontentexample/app/src/main/java/com/hasan/dynamiccontentexample/MainActivity.kt
@@ -1,24 +1,27 @@
 package com.hasan.dynamiccontentexample
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -34,42 +37,81 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen() {
+    val greetingsListState = remember {
+        mutableStateListOf("John", "Elisa", "Ed", "Lena")
+    }
+    val newNameState = remember { mutableStateOf("") }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(0.dp, 20.dp, 0.dp, 0.dp),
+            .padding(0.dp, 24.dp, 0.dp, 0.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Top
     ) {
-        GreetingList()
+        GreetingList(greetingsListState,
+            {
+                newNameState.value.let {
+                    if (it.isNotEmpty()) {
+                        greetingsListState.add(it)
+                        newNameState.value = ""
+                    }
+                }
+            }, newNameState.value, { newNameState.value = it })
+
+        RemoveItemFromList {
+            if (greetingsListState.size > 0) greetingsListState.removeAt(0)
+        }
     }
 }
 
 @Composable
-fun GreetingList() {
-    val greetingsListState = remember {
-        mutableStateListOf("John")
-    }
-
-    greetingsListState.forEach {
+fun GreetingList(
+    names: List<String>,
+    addName: () -> Unit,
+    textFieldValue: String,
+    textFieldUpdate: (newName: String) -> Unit
+) {
+    names.forEach {
         Greeting(name = it)
     }
+    TextField(
+        value = textFieldValue,
+        onValueChange = textFieldUpdate,
+        modifier = Modifier.padding(0.dp, 24.dp, 0.dp, 0.dp),
+        colors = TextFieldDefaults.textFieldColors(textColor = Color.Red)
+    )
+
     Button(
         modifier = Modifier
-            .padding(0.dp, 20.dp, 0.dp, 0.dp),
-        onClick = {
-            greetingsListState.add("android")
-            greetingsListState.add("android")
-        },
+            .padding(0.dp, 30.dp, 0.dp, 0.dp),
+        onClick = addName,
         shape = CircleShape,
         border = BorderStroke(3.dp, Color.Gray),
         colors = ButtonDefaults.buttonColors(
-            containerColor = Color.Blue,
-            contentColor = Color.White
+            containerColor = Color.Blue, contentColor = Color.White
         )
     ) {
         Text(
             text = "Add new Name",
+            fontSize = 18.sp
+        )
+    }
+}
+
+@Composable
+fun RemoveItemFromList(removeName: () -> Unit) {
+    Button(
+        onClick = removeName,
+        modifier = Modifier.padding(0.dp, 12.dp, 0.dp, 0.dp),
+        shape = RectangleShape,
+        border = BorderStroke(2.dp, Color.Black),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Transparent, contentColor = Color.Black
+        )
+    ) {
+        Text(
+            text = "Remove first Name",
             fontSize = 18.sp
         )
     }
