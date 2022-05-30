5d3ade1fa778724c9f97467bd45d552e61c362ac
Hasan Cobanoglu <hasan.cobanoglu1@outlook.com.tr>
2022-05-30 15:51:25 +0300

    play with animations

--- MvvmComposeMealzApp/app/src/main/java/com/hasan/mvvmcompose_mealzapp/ui/details/MealsDetailsScreen.kt
+++ MvvmComposeMealzApp/app/src/main/java/com/hasan/mvvmcompose_mealzapp/ui/details/MealsDetailsScreen.kt
@@ -1,84 +1,75 @@
 package com.hasan.mvvmcompose_mealzapp.ui.details
 
-import androidx.compose.animation.animateColor
-import androidx.compose.animation.core.animate
-import androidx.compose.animation.core.animateDp
 import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.modifier.modifierLocalOf
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.max
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.hasan.mvvmcompose_mealzapp.model.response.MealResponse
+import java.lang.Float.min
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MealDetailsScreen(meal: MealResponse?) {
-    var profilePicState by remember { mutableStateOf(MealsDetailsProfilePicState.Normal) }
-    val transition = updateTransition(targetState = profilePicState, label = "")
-    val imageSizeDp: Dp by transition.animateDp(label = "") {
-        it.size
-    }
-    val color: Color by transition.animateColor(label = "") {
-        it.color
-    }
-    val borderWidth: Dp by transition.animateDp(label = "") {
-        it.borderWidth
-    }
-    val buttonHeight: Dp by transition.animateDp(label = "") {
-        it.btnHeight
-    }
-
-    Column {
-        Row(modifier = Modifier.fillMaxWidth()) {
-            Card(
-                elevation = CardDefaults.cardElevation(2.dp),
-                modifier = Modifier.padding(16.dp),
-                shape = CircleShape,
-                colors =
-                CardDefaults.cardColors(containerColor = Color.Transparent),
-                border = BorderStroke(
-                    width = borderWidth,
-                    color = color
-                )
-            ) {
-                AsyncImage(
-                    model = meal?.imageUrl,
-                    contentDescription = "image big",
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier
-                        .size(imageSizeDp)
-                        .padding(8.dp)
-                        .clip(CircleShape)
-                )
+    val scrollState = rememberLazyListState()
+    val offSet = min(1f, 1 - (scrollState.firstVisibleItemScrollOffset / 500f + scrollState.firstVisibleItemIndex))
+    val size by animateDpAsState(targetValue = max(100.dp, (200.dp * offSet)))
+    Surface(color = MaterialTheme.colorScheme.background) {
+        Column {
+            Surface(shadowElevation = 4.dp) {
+                Row(modifier = Modifier.fillMaxWidth()) {
+                    Card(
+                        elevation = CardDefaults.cardElevation(2.dp),
+                        modifier = Modifier.padding(16.dp),
+                        shape = CircleShape,
+                        colors =
+                        CardDefaults.cardColors(containerColor = Color.Transparent),
+                        border = BorderStroke(
+                            width = 2.dp,
+                            color = Color.Black
+                        )
+                    ) {
+                        AsyncImage(
+                            model = meal?.imageUrl,
+                            contentDescription = "image big",
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier
+                                .size(size)
+                                .clip(CircleShape)
+                        )
+                    }
+                    Text(
+                        text = "${meal?.name}",
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .align(Alignment.CenterVertically)
+                    )
+                }
             }
-            Text(
-                text = "${meal?.name}",
-                modifier = Modifier
-                    .padding(16.dp)
-                    .align(Alignment.CenterVertically)
-            )
-        }
-        Button(modifier = Modifier
-            .align(Alignment.CenterHorizontally)
-            .height(buttonHeight),
-            onClick = {
-                profilePicState =
-                    if (profilePicState == MealsDetailsProfilePicState.Normal) MealsDetailsProfilePicState.Expanded
-                    else MealsDetailsProfilePicState.Normal
-            }) {
-            Text(text = "Change state of meal profile picture")
+            val dummyList = (0..100).map { it.toString() }
+            LazyColumn(state = scrollState) {
+                items(dummyList) { item ->
+                    Text(text = item, fontSize = 18.sp, modifier = Modifier.padding(24.dp))
+                }
+            }
         }
     }
 }
