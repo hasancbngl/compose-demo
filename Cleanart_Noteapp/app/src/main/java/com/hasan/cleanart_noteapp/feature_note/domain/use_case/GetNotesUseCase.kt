14df43c5038d5a0d4572491ef9f8d6ac9c950c5c
Hasan Cobanoglu <hasan.cobanoglu1@outlook.com.tr>
2022-05-31 17:23:29 +0300

    write get notes use case

---
+++ Cleanart_Noteapp/app/src/main/java/com/hasan/cleanart_noteapp/feature_note/domain/use_case/GetNotesUseCase.kt
@@ -0,0 +1,38 @@
+package com.hasan.cleanart_noteapp.feature_note.domain.use_case
+
+import androidx.compose.ui.text.toLowerCase
+import com.hasan.cleanart_noteapp.feature_note.domain.model.Note
+import com.hasan.cleanart_noteapp.feature_note.domain.repository.NoteRepository
+import com.hasan.cleanart_noteapp.feature_note.domain.util.NoteOrder
+import com.hasan.cleanart_noteapp.feature_note.domain.util.OrderType
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class GetNotesUseCase(
+    private val repository: NoteRepository
+) {
+    //use case should have only one function public which be called from outside
+
+    operator fun invoke(
+        order: NoteOrder = NoteOrder.Date(OrderType.Descending)
+    ): Flow<List<Note>> {
+        return repository.getNotes().map { notes ->
+            when (order.orderType) {
+                is OrderType.Ascending -> {
+                    when (order) {
+                        is NoteOrder.Title -> notes.sortedBy { it.title.lowercase() }
+                        is NoteOrder.Date -> notes.sortedBy { it.timestamp }
+                        is NoteOrder.Color -> notes.sortedBy { it.color }
+                    }
+                }
+                is OrderType.Descending -> {
+                    when (order) {
+                        is NoteOrder.Title -> notes.sortedByDescending { it.title.lowercase() }
+                        is NoteOrder.Date -> notes.sortedByDescending { it.timestamp }
+                        is NoteOrder.Color -> notes.sortedByDescending { it.color }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Cleanart_Noteapp/app/src/main/java/com/hasan/cleanart_noteapp/feature_note/domain/util/OrderType.kt
@@ -0,0 +1,6 @@
+package com.hasan.cleanart_noteapp.feature_note.domain.util
+
+sealed class OrderType() {
+    object Ascending : OrderType()
+    object Descending : OrderType()
+}
