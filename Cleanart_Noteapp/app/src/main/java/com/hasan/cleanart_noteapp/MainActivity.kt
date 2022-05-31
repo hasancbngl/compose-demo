43ce54efb49696a82269a4e144837c1b762d6d59
Hasan Cobanoglu <hasan.cobanoglu1@outlook.com.tr>
2022-05-31 10:35:48 +0300

    init notes app

---
+++ Cleanart_Noteapp/app/src/main/java/com/hasan/cleanart_noteapp/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.hasan.cleanart_noteapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.hasan.cleanart_noteapp.ui.theme.CleanArt_NoteAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CleanArt_NoteAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    CleanArt_NoteAppTheme {
+        Greeting("Android")
+    }
+}
